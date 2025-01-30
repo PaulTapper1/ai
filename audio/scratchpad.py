293a58f7e8b92c86b94e7b5c23d191ab6f8a48c2
Paul_NUGEN <paul@nugenaudio.com>
2025-01-30 19:05:09 +0000

    Work towards exporting out spectrograms for training

--- audio/scratchpad.py
+++ audio/scratchpad.py
@@ -14,17 +14,17 @@ import os
 HF_HOME = os.environ['HF_HOME']
 print(f"HF_HOME = '{HF_HOME}'")
 assert HF_HOME == "D:\\wkspaces\\ai_data\\huggingface", "You should set your HF_HOME directory in Windows environment variables before importing datasets"
+PTAU_HOME = "D:\\wkspaces\\ai_data\\ptau"
 import datasets
 from datasets import load_dataset
 import matplotlib.pyplot as plt
 import numpy as np
 import math
-# from playsound import playsound
-# from pydub import AudioSegment
-# from io import BytesIO
 import random
 import sounddevice as sd
-#import soundfile as sf
+import librosa
+import librosa.display
+from scipy import signal
 
 
 #-----------------------------------------------------------------------------
@@ -42,24 +42,11 @@ def load_from_huggingface(resource, language):
 	# see https://huggingface.co/datasets/mozilla-foundation/common_voice_11_0
 	print(f"{resource} '{language}'")
 	dataset = load_dataset(resource, language, split="train", trust_remote_code=True)
-	print(dataset)
+	print(f"num_rows={len(dataset)}")
+	#print(dataset)
 	#print(dataset[0])
 	#print(dataset[0]["audio"])
-	
 	return dataset
-
-# dataset = load_from_huggingface("mozilla-foundation/common_voice_11_0", "en"		)
-# dataset = load_from_huggingface("mozilla-foundation/common_voice_11_0", "ar"		)
-# dataset = load_from_huggingface("mozilla-foundation/common_voice_11_0", "hi"		)
-# dataset = load_from_huggingface("mozilla-foundation/common_voice_11_0", "zh-CN"	)
-# dataset = load_from_huggingface("mozilla-foundation/common_voice_11_0", "fr"		)
-# dataset = load_from_huggingface("mozilla-foundation/common_voice_11_0", "de"		)
-# dataset = load_from_huggingface("mozilla-foundation/common_voice_11_0", "ja"		)
-# dataset = load_from_huggingface("mozilla-foundation/common_voice_11_0", "ru"		)
-# dataset = load_from_huggingface("mozilla-foundation/common_voice_11_0", "es"		)
-
-dataset = load_from_huggingface("agkphysics/AudioSet", ""		)
-
 
 def make_freq_logscale():	
 	scale = np.linspace(math.log(min_freq_hz), math.log(max_freq_hz), num_freq_bins, False)
@@ -85,15 +72,8 @@ def convert_spectrogram_to_freq_scale(spectrogram, sample_rate, new_freq_scale):
 
 	return new_spectrogram
 
-def show_spectrogram_scipy(audio_data, sample_rate, block=True):
+def get_spectrogram(audio_data, sample_rate):
 	sr = sample_rate
-	import librosa
-	import librosa.display
-	import matplotlib.pyplot as plt
-	import numpy as np
-	from scipy import signal
-
-	# setup
 
 	# Calculate the Short-Time Fourier Transform (STFT)
 	stft = librosa.stft(audio_data, n_fft=n_fft, hop_length=hop_length)	# https://librosa.org/doc/main/generated/librosa.stft.html
@@ -107,12 +87,15 @@ def show_spectrogram_scipy(audio_data, sample_rate, block=True):
 	spectrogram = np.fmax(spectrogram,1e-8)
 	spectrogram_dbs = 20 * np.log10(spectrogram)	# convert to log (dBs)
 	max_dbs = np.max(spectrogram_dbs)
-	spectrogram_dbs -= max_dbs
-	spectrogram_dbs = np.fmax(spectrogram_dbs, -60)  # limit db range
+	dbs_floor = -60
+	spectrogram_dbs -= max_dbs + dbs_floor
+	spectrogram_dbs = np.fmax(spectrogram_dbs, 0)  # limit db range
+	spectrogram_dbs *= (-1/dbs_floor)
+	# results in the spectrogram being scaled and constrained between 0 and -60 dBs, then scaled to be between 1 and 0
 
 	# trim lead-in and lead-out
 	freqbins, timebins = np.shape(spectrogram_dbs)
-	min_level_for_timeslice = -40
+	min_level_for_timeslice = 0.8
 	time_start = 0
 	while np.max(spectrogram_dbs[:,time_start]) < min_level_for_timeslice:
 		time_start += 1
@@ -120,38 +103,76 @@ def show_spectrogram_scipy(audio_data, sample_rate, block=True):
 	while np.max(spectrogram_dbs[:,time_end]) < min_level_for_timeslice:
 		time_end -= 1
 	spectrogram_dbs = spectrogram_dbs[:,time_start:time_end]
-	timebins = time_end - time_start + 1
+	
+	return spectrogram_dbs
 
+def show_spectrogram(spectrogram_dbs, sample_rate, block=True):
+	freqbins, timebins = np.shape(spectrogram_dbs)
 	plt.clf()
 	plt.figure(num=1, figsize=(8, 4))
 	plt.pcolormesh(spectrogram_dbs, cmap="viridis")
 	clip_length = timebins * hop_length / sample_rate
 	plt.title(f"Length = {clip_length:.3f} secs, trimmed, {min_freq_hz}Hz - {max_freq_hz}Hz")
 	plt.colorbar(label="Decibels")
-	#plt.ylabel('Frequency [Hz]')
-	#plt.xlabel('Time [sec]')
 	plt.pause(0.2)  # pause a bit so that plots are updated
 	plt.show(block=block)
 
 def play_audio_from_dataset_item(dataset_item):
-	audio_clip = dataset[idx]["audio"]
+	audio_clip = dataset_item["audio"]
 	audio_data = audio_clip["array"]
 	sample_rate = audio_clip["sampling_rate"]
 	sd.play(audio_data, sample_rate)
 	sd.wait()  # Wait for playback to finish
 
-def visualize_data_item(dataset, idx):
-	audio_data 	= dataset[idx]["audio"]["array"]
-	sample_rate = dataset[idx]["audio"]["sampling_rate"]
-	print(f"index={idx}")
-	print(f"human_labels={dataset[idx]['human_labels']}")
-	show_spectrogram_scipy(audio_data, sample_rate, block=False)
-	play_audio_from_dataset_item(dataset[idx])
+def visualize_data_item(dataset_item, play_audio=True):
+	sample_rate = dataset_item["audio"]["sampling_rate"]
+	#print(f"sample_rate={sample_rate}")
+	if "human_labels" in dataset_item:
+		print(f"human_labels={dataset_item['human_labels']}")
+	audio_data 	= dataset_item["audio"]["array"]
+	spectrogram_dbs = get_spectrogram(audio_data, sample_rate)
+	show_spectrogram(spectrogram_dbs, sample_rate, block=False)
+	if play_audio:
+		play_audio_from_dataset_item(dataset_item)
+
+def visualize_some_data_items(dataset, num_items=10):
+	id_base = random.randint(0, len(dataset)-1-num_items)
+	for i in range(0, num_items):
+		visualize_data_item(dataset, id_base+i)
+	plt.show(block=True)
+
+def export_spectrogram_from_dataset_item(dataset_item, is_dialog):
+	if not is_dialog and "human_labels" in dataset_item:
+		discard_labels = ["Speech", "Singing", "Chant"]
+		for discard_label in discard_labels:
+			if discard_label in dataset_item['human_labels']:
+				# this item was labelled as not dialog, but, actually probably is, so discard item
+				return
+	#print(f"is_dialog = {is_dialog}")
+	visualize_data_item(dataset_item, play_audio=False)
+	#PTAU_HOME
+
+def export_spectrograms_from_dataset(dataset, is_dialog):
+	max_item = len(dataset)-1
+	for i in range(0, max_item):
+		print(f"item {i}\r")
+		export_spectrogram_from_dataset_item(dataset[i], is_dialog)
+	print("\n")
+	
+def process_dataset(dataset, is_dialog):
+	export_spectrograms_from_dataset(dataset, is_dialog)
+	#visualize_some_data_items(dataset)
+
+# process_dataset( load_from_huggingface("mozilla-foundation/common_voice_11_0", "en"		), True )
+process_dataset( load_from_huggingface("mozilla-foundation/common_voice_11_0", "ar"		), True )
+# process_dataset( load_from_huggingface("mozilla-foundation/common_voice_11_0", "hi"		), True )
+# process_dataset( load_from_huggingface("mozilla-foundation/common_voice_11_0", "zh-CN"	), True )
+# process_dataset( load_from_huggingface("mozilla-foundation/common_voice_11_0", "fr"		), True )
+# process_dataset( load_from_huggingface("mozilla-foundation/common_voice_11_0", "de"		), True )
+# process_dataset( load_from_huggingface("mozilla-foundation/common_voice_11_0", "ja"		), True )
+# process_dataset( load_from_huggingface("mozilla-foundation/common_voice_11_0", "ru"		), True )
+# process_dataset( load_from_huggingface("mozilla-foundation/common_voice_11_0", "es"		), True )
+
+# process_dataset( load_from_huggingface("agkphysics/AudioSet", ""						), False )	
 
 
-# Get the audio data from the first example
-for i in range(0, 10):
-	idx = random_number = random.randint(0, len(dataset)-1)
-	visualize_data_item(dataset, idx)
-
-plt.show(block=True)
